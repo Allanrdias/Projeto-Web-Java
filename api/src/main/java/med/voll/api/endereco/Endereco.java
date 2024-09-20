c2704a4b066b20b1404ea5073f7dfc644f05a3b5
Allan <allandias.vp@gmail.com>
2024-09-20 17:51:22 -0300

    Incluindo Projeto Web

---
+++ api/src/main/java/med/voll/api/endereco/Endereco.java
@@ -0,0 +1,30 @@
+package med.voll.api.endereco;
+
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Embeddable
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Endereco {
+    private String logradouro;
+    private String numero;
+    private String bairro;
+    private String cidade;
+    private String complemento;
+    private String cep;
+    private String uf;
+
+    public Endereco(DadosEndereco dados) {
+        this.logradouro = dados.logradouro();
+        this.bairro = dados.bairro();
+        this.cidade = dados.cidade();
+        this.complemento = dados.complemento();
+        this.cep = dados.cep();
+        this.uf = dados.uf();
+        this.numero = dados.numero();
+    }
+}
